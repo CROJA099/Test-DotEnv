5b635c05c05b2a2bc267e4575cc77a9cbfbecc56
Carlos Rojas <carlos.rojas@foundever.com>
2025-04-14 14:26:54 -0500

    UPDATE:: Build.gradle and codemagic.yaml files was updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,34 +43,39 @@ android {
     
 
     signingConfigs {
-        create("mazdaConfig") {
-            if (System.getenv("CI") != null) {
-                storeFile = file(System.getenv("MAZDA_KEYSTORE_PATH"))
-                storePassword = System.getenv("MAZDA_KEYSTORE_PASSWORD")
-                keyAlias = System.getenv("MAZDA_KEY_ALIAS")
-                keyPassword = System.getenv("MAZDA_KEY_PASSWORD")
-            } else {
-                storeFile = keystoreProperties["mazda_storeFile"]?.let { file(it as String) }
-                storePassword = keystoreProperties["mazda_storePassword"] as String?
-                keyAlias = keystoreProperties["mazda_keyAlias"] as String?
-                keyPassword = keystoreProperties["mazda_keyPassword"] as String?
+        if (project.hasProperty("mazdaBuild")) {
+            create("mazdaConfig") {
+                if (System.getenv("CI") != null) {
+                    storeFile = file(System.getenv("MAZDA_KEYSTORE_PATH") ?: throw GradleException("MAZDA_KEYSTORE_PATH is not defined"))
+                    storePassword = System.getenv("MAZDA_KEYSTORE_PASSWORD") ?: throw GradleException("MAZDA_KEYSTORE_PASSWORD is not defined")
+                    keyAlias = System.getenv("MAZDA_KEY_ALIAS") ?: throw GradleException("MAZDA_KEY_ALIAS is not defined")
+                    keyPassword = System.getenv("MAZDA_KEY_PASSWORD") ?: throw GradleException("MAZDA_KEY_PASSWORD is not defined")
+                } else {
+                    storeFile = keystoreProperties["mazda_storeFile"]?.let { file(it as String) }
+                    storePassword = keystoreProperties["mazda_storePassword"] as String?
+                    keyAlias = keystoreProperties["mazda_keyAlias"] as String?
+                    keyPassword = keystoreProperties["mazda_keyPassword"] as String?
+                }
             }
         }
 
-        create("nissanConfig") {
-            if (System.getenv("CI") != null) {
-                storeFile = file(System.getenv("NISSAN_KEYSTORE_PATH"))
-                storePassword = System.getenv("NISSAN_KEYSTORE_PASSWORD")
-                keyAlias = System.getenv("NISSAN_KEY_ALIAS")
-                keyPassword = System.getenv("NISSAN_KEY_PASSWORD")
-            } else {
-                storeFile = keystoreProperties["nissan_storeFile"]?.let { file(it as String) }
-                storePassword = keystoreProperties["nissan_storePassword"] as String?
-                keyAlias = keystoreProperties["nissan_keyAlias"] as String?
-                keyPassword = keystoreProperties["nissan_keyPassword"] as String?
+        if (project.hasProperty("nissanBuild")) {
+            create("nissanConfig") {
+                if (System.getenv("CI") != null) {
+                    storeFile = file(System.getenv("NISSAN_KEYSTORE_PATH") ?: throw GradleException("NISSAN_KEYSTORE_PATH is not defined"))
+                    storePassword = System.getenv("NISSAN_KEYSTORE_PASSWORD") ?: throw GradleException("NISSAN_KEYSTORE_PASSWORD is not defined")
+                    keyAlias = System.getenv("NISSAN_KEY_ALIAS") ?: throw GradleException("NISSAN_KEY_ALIAS is not defined")
+                    keyPassword = System.getenv("NISSAN_KEY_PASSWORD") ?: throw GradleException("NISSAN_KEY_PASSWORD is not defined")
+                } else {
+                    storeFile = keystoreProperties["nissan_storeFile"]?.let { file(it as String) }
+                    storePassword = keystoreProperties["nissan_storePassword"] as String?
+                    keyAlias = keystoreProperties["nissan_keyAlias"] as String?
+                    keyPassword = keystoreProperties["nissan_keyPassword"] as String?
+                }
             }
         }
     }
+
 
     flavorDimensions("flavor-type")
     productFlavors {
